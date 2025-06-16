e8ed818c2a8a3c2a74269049f4a6fa1306f10352
Алексей <olseky2610@mail.ru>
2025-06-16 14:27:37 +0300

    Fixed commands

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,6 +1,5 @@
 from .base import router as base_router
-from .habits_buttons import router as habits_buttons_router
-from .habits_commands import router as habits_commands_router
+from .habits_buttons import router as habits_btn_router
+from .habits_commands import router as habits_cmd_router
 
-routers = [base_router, habits_buttons_router, habits_commands_router]
-
+routers = [base_router, habits_btn_router, habits_cmd_router]
--- bot/handlers/habits_commands.py
+++ bot/handlers/habits_commands.py
@@ -6,28 +6,28 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message, CallbackQuery
 
-from bot.service.database import delete_habit, get_habit, get_habits, add_habit
+from bot.service import database
 
 logger = logging.getLogger("HabitBot")
 
 router = Router(name="habits_commands")
 
 
-@router.message(Command("/del"))
+@router.message(Command("del"))
 async def del_habit(message: Message, command: CommandObject):
     habit = command.args
     if not habit:
         await message.answer("Формат: /del <название привычки>")
         return
 
-    resp = await delete_habit(message.from_user.id, habit)
+    resp = await database.delete_habit(message.from_user.id, habit)
     if resp:
         await message.answer(f"Привычка {resp} удалена")
     else:
         await message.answer(f"У вас нет привычки {habit}")
 
 
-@router.message(Command("/add"))
+@router.message(Command("add"))
 async def add_habit(message: Message, command: CommandObject):
     logger.info("Зашел в /add")
     try:
@@ -37,23 +37,23 @@ async def add_habit(message: Message, command: CommandObject):
         name, count = habit.rsplit(" ", 1)
         count = int(count)
 
-        resp = await get_habit(message.from_user.id, name)
+        resp = await database.get_habit(message.from_user.id, name)
         if resp:
             await message.answer(f"Привычка {name} уже существует!")
             return
 
-        await add_habit(message.from_user.id, name, count)
+        await database.add_habit(message.from_user.id, name, count)
         await message.answer(f"Привычка {name} добавлена!")
     except Exception as e:
         await message.answer(f"Формат: /add <название> <кол-во повторений>")
         logger.error(e)
 
 
-@router.message(Command("/list"))
+@router.message(Command("list"))
 async def list_habit(message: Message):
-    habits = await get_habits(message.from_user.id)
+    habits = await database.get_habits(message.from_user.id)
     if not habits:
         await message.answer("У вас нет привычек!")
         return
     text = "\n".join([habit[1] for habit in habits])
-    await callback.message.edit_text("Ваши привычки:\n" + text)
+    await message.answer("Ваши привычки:\n" + text)
